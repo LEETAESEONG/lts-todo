373e772aade6dd1ae6be18e1030c9ec93ef1d164
이태성 <ga1754@naver.com>
2024-09-05 01:25:49 +0900

    [feat] layout 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,113 @@
-import "./App.css";
+import React from "react";
+import {
+  AppstoreOutlined,
+  BarChartOutlined,
+  CloudOutlined,
+  ShopOutlined,
+  TeamOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from "@ant-design/icons"; // 아이콘 import
+import { Layout, Menu, theme } from "antd"; // Ant Design 레이아웃과 테마 import
 
-function App() {
+const { Header, Content, Footer, Sider } = Layout; // 레이아웃 구성 요소
+const siderStyle: React.CSSProperties = {
+  overflow: "auto",
+  height: "100vh",
+  position: "fixed",
+  insetInlineStart: 0,
+  top: 0,
+  bottom: 0,
+  scrollbarWidth: "thin",
+  scrollbarColor: "unset",
+};
+
+// 사이드 메뉴 아이템 설정
+const items = [
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+  BarChartOutlined,
+  CloudOutlined,
+  AppstoreOutlined,
+  TeamOutlined,
+  ShopOutlined,
+].map((icon, index) => ({
+  key: String(index + 1),
+  icon: React.createElement(icon),
+  label: `nav ${index + 1}`,
+}));
+
+const App = () => {
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken(); // 테마 토큰 사용
   return (
-    <div>
-      <h1>app</h1>
-    </div>
+    <Layout hasSider>
+      <Sider style={siderStyle}>
+        <div className="demo-logo-vertical" />
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={["4"]}
+          items={items}
+        />
+      </Sider>
+      <Layout
+        style={{
+          marginInlineStart: 200,
+          minHeight: "100vh",
+        }}
+      >
+        <Header
+          style={{
+            padding: 0,
+            background: colorBgContainer,
+          }}
+        />
+        <Content
+          style={{
+            margin: "24px 16px 0",
+            overflow: "initial",
+          }}
+        >
+          <div
+            style={{
+              padding: 24,
+              textAlign: "center",
+              background: colorBgContainer,
+              borderRadius: borderRadiusLG,
+              minHeight: "60vh",
+            }}
+          >
+            <p>long content</p>
+            {/* {
+              // 긴 컨텐츠 표시
+              Array.from(
+                {
+                  length: 100,
+                },
+                (_, index) => (
+                  <React.Fragment key={index}>
+                    {index % 20 === 0 && index ? "more" : "..."}
+                    <br />
+                  </React.Fragment>
+                )
+              )
+            } */}
+          </div>
+        </Content>
+        <Footer
+          style={{
+            textAlign: "center",
+          }}
+        >
+          Ant Design ©{new Date().getFullYear()} Created by Ant UED
+        </Footer>
+      </Layout>
+    </Layout>
   );
-}
+};
 
 export default App;
